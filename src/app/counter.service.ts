debe25d5f2617b8b9e69f96245a6bf8108ede5c4
Laxmi Sharma <sharma.laxmi247@gmail.com>
2020-04-19 21:57:29 +0530

    initial commit of assignment 5: usage of services and dependency injection

---
+++ src/app/counter.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class CounterService{
+activeToInactiveCounter=0;
+inactiveToActiveCounter=0;
+
+incrementActiveToInactiveCounter(){
+    this.activeToInactiveCounter++;
+    console.log("Active to inactive : "+this.activeToInactiveCounter);
+}
+incrementInactiveToActiveCounter(){
+this.inactiveToActiveCounter++;
+console.log("Inactive to active : "+this.inactiveToActiveCounter);
+}
+}
